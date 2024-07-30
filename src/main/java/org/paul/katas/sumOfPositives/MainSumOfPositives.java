e754bd0aef5f7b3e10d0cf48a13d8280938fd2c4
whalleyph <walypa56@gmail.com>
2024-07-30 14:12:00 +0100

    Initial commit

---
+++ src/main/java/org/paul/katas/sumOfPositives/MainSumOfPositives.java
@@ -0,0 +1,13 @@
+package org.paul.katas.sumOfPositives;
+
+public class MainSumOfPositives {
+    int sumOfPositives(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0){
+                sum += arr[i];
+            }
+        }
+        return sum;
+    }
+}
